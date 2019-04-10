f95da1cd4729d4b34ea16c9fe29cc3cc19c56b95
Duy <hi.am.not.me@gmail.com>
2019-04-10 22:18:05 +0700

    Add api/, fix app.js

--- app.js
+++ app.js
@@ -2,42 +2,43 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-var database = require('./routes/database');
+// var database = require('./routes/database');
 
 var indexRouter = require('./routes/index');
 var testPage = require('./routes/testPage');
+var lobbyRouter = require('./routes/lobby');
+
+var testAPI = require('./api/test');
 
 app.use(express.static("public"));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+// Routes
 // use res.render to load up an ejs view file
 app.set('views', __dirname + '/views');
-
-// TEST: Getting data on realtime database
-app.get('/test', function (req, res) {
-  database.ref('/Rooms/r001').once('value').then(function(snapshot) {
-    var data = snapshot.val();
-    res.send({room: data});
-  });
-});
 
 app.use('/', indexRouter);
 
 app.use('/testPage', testPage);
 
-var lobbyCreator = require('./scripts/createLobby');
-app.get('/newlobby', function(req, res) {
-  // var ID = (Math.floor(Math.random() * 1001)).toString();
-  lobbyCreator.createLobby(database ,res);
-});
+app.use('/lobby', lobbyRouter);
 
-app.get('/lobby/:id', function(req, res) {
-  var id = req.params['id'];
-  database.ref('/Rooms/' + id).set({status: 'lobby'});
-  res.render('lobby', {lobbyID: id});
-});
+// API
+app.use('/test', testAPI);
+
+// var lobbyCreator = require('./scripts/createLobby');
+// app.get('/newlobby', function(req, res) {
+//   // var ID = (Math.floor(Math.random() * 1001)).toString();
+//   lobbyCreator.createLobby(database ,res);
+// });
+
+// app.get('/lobby/:id', function(req, res) {
+//   var id = req.params['id'];
+//   database.ref('/Rooms/' + id).set({status: 'lobby'});
+//   res.render('lobby', {lobbyID: id});
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
