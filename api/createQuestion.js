0ff3f41efc992978fead74eb89b3a87b875f7851
Duy <hi.am.not.me@gmail.com>
2019-05-05 16:55:42 +0700

    Fix re-auth with create question

--- api/createQuestion.js
+++ api/createQuestion.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 var database = require('../routes/database');
 
-router.post('/', function (req, res) {
-    var data = req;
-    console.log(JSON.stringify(data));
+router.post('/', function(req, res, next) {
+    var data = req.body;
+    console.log("CREATE PACKS:" + JSON.stringify(data));
+
+
+
+    res.send({response: "OK"});
 });
 
 module.exports = router;
